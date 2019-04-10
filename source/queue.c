59a7e14abf546254f12ca785aabee194442edcf5
maxmrmr <maxmr@stud.ntnu.no>
2019-04-10 13:43:30 +0200

    Fant en bug til med queue_check_current_floor, men har fikset det nå. tror det ble litt kluss etter at du byttet tilbake fra regnestykker til tall

--- source/queue.c
+++ source/queue.c
@@ -120,35 +120,38 @@ int queue_check_orders_at_current_floor(int current_floor, int last_direction){
 
 	if (last_direction < 0){
 		if (current_floor == 2){
-			if ( (queue[3]!=1) && (queue[0]!=1) ){
-				if (queue[6])
-					return 1;
+			if ( (!queue[0]) && (!queue[1]) && (!queue[2]) ){  		//Check if no orders in 1st floor
+				if ( (!queue[3]) && (!queue[4]) && (!queue[5]) ){ 	//Check if no orders in 2nd floor
+					if (queue[6]){									//Check if order in 3rd floor, not in same direction or command
+						return 1;
+					}
+				}
 			}
 		}
 
 		if (current_floor == 1){
-			if (queue[0]!=1){
-				if (queue[3])
+			if ( (!queue[0]) && (!queue[1]) && (!queue[2]) ){ 	//Check if no orders in 1st floor
+				if (queue[3]){									//Check if order in 2nd floor, not in same direction or command
 					return 1;
+				}
 			}
 		}
 	}
 	if (last_direction > 0){
 		if (current_floor == 1){
-			if ( (queue[7]!=1) && (queue[10]!=1) ){
-				if ( (queue[8]!=1) && (queue[11]!=1) )
-					if (queue[4]){
+			if ( (!queue[6]) && (!queue[7]) && (queue[8]!=1) ){ 	//Check if no orders in 3rd floor
+				if ( (!queue[9]) && (!queue[10]) && (!queue[11]) ){ //Check if no orders in 4th floor
+					if (queue[4]){									//Check if order in 2nd floor, not in same direction or command
 						return 1;
 					}
+				}
 			}
 		}
 
 		if (current_floor == 2){
-			if (queue[10]!=1){
-				if (queue[11]!=1){
-					if (queue[7]){
-						return 1;
-					}
+			if ( (!queue[9]) &&	(!queue[10]) && (!queue[11]) ){ 	//Check if no orders in 4th floor
+				if (queue[7]){									//Check if order in 3rd floor, not in same direction or command
+					return 1;
 				}
 			}
 		}
@@ -158,24 +161,24 @@ int queue_check_orders_at_current_floor(int current_floor, int last_direction){
 
 void queue_clear_order(int last_floor){
 	if (last_floor == 0){
-		queue[0] = 0;
-		queue[1] = 0;	//dummy
-		queue[2] = 0;
+		queue[0] = 0;	//1st floor CALL_UP
+		queue[1] = 0;	//Dummy
+		queue[2] = 0;	//1st floor COMMAND
 	}
 	else if (last_floor == 1){
-		queue[3] = 0;
-		queue[4] = 0;
-		queue[5] = 0;
+		queue[3] = 0;	//2nd floor CALL_UP
+		queue[4] = 0;	//2nd floor CALL_DOWN
+		queue[5] = 0;	//2nd floor COMMAND
 	}
 	else if (last_floor == 2){
-		queue[6] = 0;
-		queue[7] = 0;
-		queue[8] = 0;
+		queue[6] = 0;	//3rd floor CALL_UP
+		queue[7] = 0;	//3rd floor CALL_DOWN
+		queue[8] = 0;	//3rd floor COMMAND
 	}
 	else if (last_floor == 3){
-		queue[9] = 0;	//dummy
-		queue[10] = 0;
-		queue[11] = 0;
+		queue[9] = 0;	//Dummy
+		queue[10] = 0;	//4th floor CALL_DOWN
+		queue[11] = 0;	//4th floor COMMAND
 	}
 }
 
